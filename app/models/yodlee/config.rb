3c3533efd57480ea8e92e07fe25aedc3222c9366
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-04-27 23:07:33 -0400

    login and capture session token

--- app/models/yodlee/config.rb
+++ app/models/yodlee/config.rb
@@ -1,14 +1,14 @@
 module Yodlee
   class Config
 
-    class_attribute :username, :password, :base_url, :register_users
+    class_attribute :username, :password, :register_users, :base_url, :cobrand_session_token_url
 
     # Load YAML settings
     YAML.load_file("#{Rails.root}/config/yodlee.yml")[Rails.env].each do |key, value|
-      self.senc("#{key}=", value)
+      self.send("#{key}=", value)
     end
 
-    Yodlee::Base.base_url base_url
+    Yodlee::Base.base_uri base_url
 
   end
 end
